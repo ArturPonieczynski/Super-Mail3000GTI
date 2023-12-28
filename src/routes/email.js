54cdc459111e5dc3cd64e4c37caa7ee4c14c0511
Artur Ponieczynski <ponieczynski@gmail.com>
2023-12-28 16:03:20 +0100

    Refactor/email-service: Add error validation when fetching emails list.

--- src/routes/email.js
+++ src/routes/email.js
@@ -11,11 +11,11 @@ mailRouter.get('/all', async (req, res) => {
         const memberList = await MemberRecord.findAll();
         res.json(memberList);
     } catch (err) {
-
+        throw new ValidationError('Błąd podczas ładowania książki adresów.')
     }
-})
+});
 
-mailRouter.post('/', upload.single('file'),  (req, res) => {
+mailRouter.post('/', upload.single('file'), (req, res) => {
     try {
         EmailService.sendMailService(req);
         res.json({ok: true});

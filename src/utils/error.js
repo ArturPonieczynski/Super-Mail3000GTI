754020d147c3d103c2bcaf62a97594b5d9b438a9
Artur Ponieczynski <ponieczynski@gmail.com>
2024-01-03 22:38:48 +0100

    Refactor/email-service: Refactor error handling.

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,20 +1,20 @@
 export class ValidationError extends Error {}
+
 export class NotFoundError extends Error {}
 
 export const handleError = (error, req, res, next) => {
     if (error instanceof NotFoundError) {
         res
             .status(404)
-            .json({
-                error: error.message,
-            });
-        return;
+            .json({error: error.message});
+    } else if (error instanceof ValidationError) {
+        res
+            .status(400)
+            .json({error: error.message});
+    } else {
+        console.error(error);
+        res
+            .status(500)
+            .json({error: 'Sorry, an error occurred. Try again later.'});
     }
-
-    console.error(error);
-    res
-        .status(error instanceof ValidationError ? 400 : 500)
-        .json({
-            error: error instanceof ValidationError ? error.message : 'Sorry error, try again later.',
-        });
 };

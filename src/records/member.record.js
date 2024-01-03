e320730d82f04f9c9b1e5a386c2114e46a278bd3
Artur Ponieczynski <ponieczynski@gmail.com>
2024-01-03 21:45:42 +0100

    Refactor/email-service: Add regex to email validation function, check if input = string. Update all async methods of try/catch block.

--- src/records/member.record.js
+++ src/records/member.record.js
@@ -12,44 +12,75 @@ export class MemberRecord {
     }
 
     validate(obj) {
-        if (!obj.email || obj.email.length > 50) {
+
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (
+            !obj.email ||
+            typeof obj.email !== 'string' ||
+            !emailRegex.test(obj.email) ||
+            obj.email.length > 50
+        ) {
             throw new ValidationError('E-mail jest nieprawidłowy. Podaj poprawny e-mail i nie przekraczaj 50 znaków.');
         }
-        if (obj.description.length > 45) {
+        if (typeof obj.description !== 'string' || obj.description.length > 45) {
             throw new ValidationError('Opis jest za długi. Możesz użyć maksymalnie 45 znaków.')
         }
     }
 
     async insert() {
-        await pool.execute("INSERT INTO `members` (`id`,`email`, `description`) VALUES (:id, :email, :description)", {
-            id: this.id,
-            email: this.email,
-            description: this.description,
-        });
-        return this.id;
+       try {
+           await pool.execute("INSERT INTO `members` (`id`,`email`, `description`) VALUES (:id, :email, :description)", {
+               id: this.id,
+               email: this.email,
+               description: this.description,
+           });
+
+           return this.id;
+       } catch (error) {
+           console.error('Error occurred during member insertion:', error);
+           throw new Error('Nie udało się dodać nowego adresu e-mail.');
+       }
     }
 
     static async delete(id) {
-        const member = await this.findOne(id);
-        if (member) {
-            const result = await pool.execute("DELETE FROM `members` WHERE `id` = :id", {
-                id: id,
-            });
-            return result[0].affectedRows;
-        } else {
-            return 0;
+        try {
+            const member = await this.findOne(id);
+            if (member) {
+                const result = await pool.execute("DELETE FROM `members` WHERE `id` = :id", {
+                    id: id,
+                });
+                return result[0].affectedRows;
+            } else {
+                return 0;
+            }
+        } catch (error) {
+            console.error('Error occurred during member deletion:', error);
+            throw new Error('Nie udało się usunąć adresu e-mail.');
         }
     }
 
     static async findOne(id) {
-        const [result] = await pool.execute("SELECT * FROM `members` WHERE `id` = :id", {
-            id: id,
-        });
-        return result.length === 0 ? null : new MemberRecord(...result);
+        try {
+            const [result] = await pool.execute("SELECT * FROM `members` WHERE `id` = :id", {
+                id: id,
+            });
+            return result.length === 0 ? null : new MemberRecord(...result);
+
+        } catch (error) {
+            console.error('Error occurred during member search by id:', error);
+            throw new Error(`Nie udało się lub nie znaleziono adresu e-mail.`);
+        }
     }
 
     static async findAll() {
-        const [result] = await pool.execute("SELECT * FROM `members` ORDER BY `description` ASC");
-        return result.map(obj => new MemberRecord(obj));
+        try {
+            const [result] = await pool.execute("SELECT * FROM `members` ORDER BY `description` ASC");
+            return result.map(obj => new MemberRecord(obj));
+
+        } catch (error) {
+            console.error('Error occurred during member search all:', error);
+            throw new Error('Nie udało się pobrać adresów e-mail z bazy danych.');
+        }
     };
 }

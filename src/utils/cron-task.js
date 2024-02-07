53a264b8ebcb04491755ebc89fc4647c283671a5
Artur Ponieczynski <ponieczynski@gmail.com>
2024-02-07 23:58:56 +0100

    Feature/delete-old-files: Create logic that execute witch file is older than maxAge.

--- src/utils/cron-task.js
+++ src/utils/cron-task.js
@@ -1,10 +1,22 @@
 import {readdir} from 'fs/promises';
 
-// const maxAge = 1000 * 60 * 60 * 24 * 400;
+const maxAge = 1000 * 60 * 60 * 24 * 400;
 const deleteOldFiles = async () => {
 
     const files = await readdir('../uploads');
-    console.log(files);
+
+    files.forEach(file => {
+        const [dateStampString] = file.split('-');
+        const now = Date.now();
+
+        if (now - Number(dateStampString) > maxAge) {
+            console.log('delete old file');
+        } else {
+            console.log('leave file');
+        }
+        // console.log(dateStampString);
+    })
+    // console.log(files);
 
     // const now = Date.now();
 };

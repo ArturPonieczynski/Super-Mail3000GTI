ac83af97979745da02a773777768164b48307cf8
Artur Ponieczynski <ponieczynski@gmail.com>
2024-03-25 01:17:52 +0100

    release-1.4.0: Bug fix in is-production.js file.

--- src/utils/is-production.js
+++ src/utils/is-production.js
@@ -3,5 +3,5 @@ import {config} from '../config.js';
 const {APP_ENV} = config;
 
 export const isProductionYesNo = (envVariable, developVariable) => {
-return  APP_ENV === 'production' ? config[envVariable] : developVariable;
+return  APP_ENV === 'production' ? envVariable : developVariable;
 }
